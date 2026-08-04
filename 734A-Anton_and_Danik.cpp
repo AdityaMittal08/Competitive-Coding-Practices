b97915bf875f8dca5fc384560cf06f5d28703e42
AdityaMittal08 <mittaladitya0801@gmail.com>
2026-08-04 23:53:09 +0530

    734A Anton and Danik

---
+++ 734A-Anton_and_Danik.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    string s;
+    cin >> s; 
+
+    int anton = 0;
+    int danik = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'A') {
+            anton++;
+        } else {
+            danik++;
+        }
+    }
+
+    if (anton > danik) {
+        cout << "Anton";
+    } else if (danik > anton) {
+        cout << "Danik";
+    } else {
+        cout << "Friendship";
+    }
+
+    return 0;
+}
